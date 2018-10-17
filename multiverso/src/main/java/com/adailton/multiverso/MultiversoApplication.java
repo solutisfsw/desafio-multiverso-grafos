f0312770d604cd9e6a647c2ad0f6030d2988d87c
ADAILTON <adailton.oliveira90@gmail.com>
2018-10-17 00:06:48 -0300

    Criação das classes Vertices e Arestas

---
+++ multiverso/src/main/java/com/adailton/multiverso/MultiversoApplication.java
@@ -0,0 +1,41 @@
+package com.adailton.multiverso;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.adailton.multiverso.entity.Aresta;
+import com.adailton.multiverso.entity.UniversoVertice;
+
+@SpringBootApplication
+public class MultiversoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiversoApplication.class, args);
+		
+		UniversoVertice a = new UniversoVertice("a");
+		UniversoVertice b = new UniversoVertice("b");
+		UniversoVertice c = new UniversoVertice("c");
+		UniversoVertice d = new UniversoVertice("d");
+		UniversoVertice e = new UniversoVertice("e");
+		
+		//adicionando as Arestas para cada classe
+		a.setRotas(Arrays.asList(new Aresta(70, e),
+								new Aresta(50, b),
+								new Aresta(50, d) ));
+		
+		b.setRotas(Arrays.asList(new Aresta(40, c)));
+		
+		c.setRotas(Arrays.asList(new Aresta(20, e),
+								new Aresta(40, d)));
+		
+		d.setRotas(Arrays.asList(new Aresta(40, c),
+								new Aresta(80, e)));
+		e.setRotas(Arrays.asList(new Aresta(30, b)));
+		
+		
+		
+				
+	}
+}
---
+++ multiverso/src/main/java/com/adailton/multiverso/entity/Aresta.java
@@ -0,0 +1,38 @@
+package com.adailton.multiverso.entity;
+
+public class Aresta {
+	private int espaco_tempo;
+	private UniversoVertice destino;
+	private int visitado = 0;
+	
+	
+	
+	public Aresta(int espaco_tempo, UniversoVertice destino) {
+		super();
+		this.espaco_tempo = espaco_tempo;
+		this.destino = destino;
+	}
+	public int getespaco_tempo() {
+		return espaco_tempo;
+	}
+	public void setespaco_tempo(int espaco_tempo) {
+		this.espaco_tempo = espaco_tempo;
+	}
+	public UniversoVertice getDestino() {
+		return destino;
+	}
+	public void setDestino(UniversoVertice destino) {
+		this.destino = destino;
+	}
+	public int getVisitado() {
+		return visitado;
+	}
+	public void setVisitado(int visitado) {
+		this.visitado = visitado;
+	}
+	
+	
+	
+	
+	
+}
