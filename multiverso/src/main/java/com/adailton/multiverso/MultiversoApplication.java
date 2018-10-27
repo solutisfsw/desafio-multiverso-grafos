75133967e8801279cbbfb0cd6aef0754f3b27d59
ADAILTON <adailton.oliveira90@gmail.com>
2018-10-27 00:12:59 -0300

    Metodo de calcular menor caminho e menu dropdown para escolha de universos

--- multiverso/src/main/java/com/adailton/multiverso/MultiversoApplication.java
+++ multiverso/src/main/java/com/adailton/multiverso/MultiversoApplication.java
@@ -18,34 +18,7 @@ public class MultiversoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiversoApplication.class, args);
+			
 		
-
-		ListaDeVertice lista = new ListaDeVertice();
-		lista.iniciaVertices();
-		lista.iniciaListaVertice();
-		
-		String b1= "b";
-		String b2= "b";
-		
-		UniversoVertice origem = lista.convertStringVertice(b1);
-		UniversoVertice destino = lista.convertStringVertice(b2);
-		
-		CalculosMultiverso calculo = new CalculosMultiverso();
-		List<Caminho> cam = new ArrayList<>();//criando lista para empilhar os caminhos
-		
-		calculo.calculaCaminhoPrincipal(origem, destino, cam);//entradas de testes
-		calculo.menorCaminho(cam);
-		
-		System.out.println("total de caminhos possiveis: " + cam.size());
-		System.out.println("\n");
-		for(int i=0; i<cam.size();i++) {
-			for(i=0; i<cam.size();i++) {
-				System.out.println("qtd de paradas: "+ cam.get(i).getQtdParada());
-				System.out.println("qtd total do percurso: " + cam.get(i).getTotalEspacoTempo());
-				System.out.println(cam.get(i).getLetrasDoCaminho());
-				System.out.println(cam.get(i).getMenorCaminho());
-				System.out.println("*************************");
-			}
-		}
 	}
 }
--- multiverso/src/main/java/com/adailton/multiverso/entity/CalculosMultiverso.java
+++ multiverso/src/main/java/com/adailton/multiverso/entity/CalculosMultiverso.java
@@ -9,7 +9,10 @@ public class CalculosMultiverso {
 	public void calculaCaminhoPrincipal(UniversoVertice verticeOrigem, UniversoVertice verticeDestino,List<Caminho> listaCaminho) {
 		if(verticeDestino.getNome().equalsIgnoreCase("b")) {
 			calculaCaminhoBB(verticeOrigem, verticeDestino, listaCaminho);
-		}else {
+		}else if(verticeDestino.getNome().equalsIgnoreCase("e")) {
+			calculaCaminhoEE(verticeOrigem, verticeDestino, listaCaminho);
+		}
+		else {
 			calculaCaminhoNormal(verticeOrigem, verticeDestino, listaCaminho);
 		}
 	}
@@ -163,16 +166,81 @@ public class CalculosMultiverso {
 			}
 		}
 	}
+public void calculaCaminhoEE(UniversoVertice verticeOrigem, UniversoVertice verticeDestino,List<Caminho> listaCaminho) {
+		
+		
+		for(Aresta v:verticeOrigem.getRotas()) {
+			Caminho cam = new Caminho() ;//por conta disso não consegui fazer recursividade
+			if(!cam.isCaminhoValido()) {
+				if(v.getDestino().getNome().equalsIgnoreCase(verticeDestino.getNome())) {
+					cam.setCaminhoValido(true);
+					cam.somaTotalEP(v.getespaco_tempo());
+					cam.setletrasDoCaminho(v.getDestino().getNome());
+					cam.setQtdParada(cam.getLetrasDoCaminho().size());
+					listaCaminho.add(cam);				
+				}else {
+					cam.somaTotalEP(v.getespaco_tempo());
+					cam.setletrasDoCaminho(v.getDestino().getNome());
+					UniversoVertice verticeAuxiliar = v.getDestino();
+					
+					for(Aresta v2:verticeAuxiliar.getRotas()) {//segundo nó
+						if(!cam.isCaminhoValido()) {
+							if(v2.getDestino().getNome().equalsIgnoreCase(verticeDestino.getNome())) {
+								cam.setCaminhoValido(true);
+								cam.somaTotalEP(v2.getespaco_tempo());
+								cam.setletrasDoCaminho(v2.getDestino().getNome());
+								cam.setQtdParada(cam.getLetrasDoCaminho().size());
+								listaCaminho.add(cam);
+							}else {
+								cam.somaTotalEP(v2.getespaco_tempo());
+								cam.setletrasDoCaminho(v2.getDestino().getNome());
+								UniversoVertice verticeAuxiliar3 = v2.getDestino();
+								
+								for(Aresta v3:verticeAuxiliar3.getRotas()) {//terceiro nó
+									if(!cam.isCaminhoValido() && v3.getespaco_tempo() == 20) {
+										if(v3.getDestino().getNome().equalsIgnoreCase(verticeDestino.getNome())) {
+											cam.setCaminhoValido(true);
+											cam.somaTotalEP(v3.getespaco_tempo());
+											cam.setletrasDoCaminho(v3.getDestino().getNome());
+											cam.setQtdParada(cam.getLetrasDoCaminho().size());
+											listaCaminho.add(cam);													
+										}else {
+											cam.somaTotalEP(v3.getespaco_tempo());
+											cam.setletrasDoCaminho(v3.getDestino().getNome());
+											UniversoVertice verticeAuxiliar4 = v3.getDestino();
+											
+											for(Aresta v4:verticeAuxiliar4.getRotas()) {
+												if(!cam.isCaminhoValido()) {
+													if(v4.getDestino().getNome().equalsIgnoreCase(verticeDestino.getNome())) {
+														cam.setCaminhoValido(true);
+														cam.somaTotalEP(v4.getespaco_tempo());
+														cam.setletrasDoCaminho(v4.getDestino().getNome());
+														cam.setQtdParada(cam.getLetrasDoCaminho().size());
+														listaCaminho.add(cam);															
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}	
+					}		
+				}
+			}
+		}
+	}
 	
 	public void menorCaminho(List<Caminho> listaCaminho) {
-		int menor = 0;
+		int menor = 700;
+		int indice = 0;
 		for(int i=0; i<listaCaminho.size();i++) {
-			if(listaCaminho.get(i).getTotalEspacoTempo()<menor) {
-				listaCaminho.get(i).setMenorCaminho("VIAGEM MAIS RÁPIDA");
+			if(listaCaminho.get(i).getTotalEspacoTempo()<=menor) {
 				menor = listaCaminho.get(i).getTotalEspacoTempo();
-			}
-			menor = listaCaminho.get(i).getTotalEspacoTempo();
+				indice = i;
+			}			
 		}
+		listaCaminho.get(indice).setMenorCaminho("VIAGEM RÁPIDA");
 	}
 }
 		
