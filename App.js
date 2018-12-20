65b46a80fc43353526fb7c7bc9ed5a59d0478830
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2018-12-20 03:03:06 -0200

    changed method to show answer

--- App.js
+++ App.js
@@ -28,190 +28,187 @@ function startApp(){
 	
 	//A distância de A a C passando por B?
 	way = Start(a, c);
-	console.log("Q1 - A distância de A a C passando por B?");
-	for(var i = 0; i < way[0].length; i++){
-		for(var j = 0; j < way[0][i].length; j++){
-			if(way[0][i][j] == b){
-				var str = "caminho "+ i + " (";
-				for(var l = 0; l < way[0][i].length-1; l++){
-					str += way[0][i][l].getLabel()+", ";
-				}
-				str += way[0][i][way[0][i].length-1].getLabel();
-				
-				str += ") possui a distancia de: "+ way[1][i];
-				
-				console.log( str );
-			}
-		}
-	}
+	str = "";
+	way_temp = getRouteToYPassingX(way, c, b);
+	document.getElementById("q1").innerHTML =  printRoutes(way_temp) ;
+	
+	
 	//A distância entre A e D?
 	way = Start(a, d);
-
-	console.log("Q2 - A distância entre A e D?");
+	str = "";
 	for(var i = 0; i < way[0].length; i++){
 		
 		if(way[0][i][ way[0][i].length-1 ] != d)
 		  continue;
-
-		for(var j = 0; j < way[0][i].length; j++){
-				var str = "caminho "+ i + " (";
-				for(var l = 0; l < way[0][i].length-1; l++){
-					str += way[0][i][l].getLabel()+", ";
-				}
-				str += way[0][i][way[0][i].length-1].getLabel();
-				
-				str += ") possui a distancia de: "+ way[1][i];
-				
-				
-			
-		}
 		
-		console.log( str );
+		str += "caminho "+ i + " (";
+		for(var j = 0; j < way[0][i].length-1; j++){
+				
+				//for(var l = 0; l < way[0][i].length-1; l++){
+					str += way[0][i][j].getLabel()+", ";
+				//}
+				//				
+		}
+		str += way[0][i][way[0][i].length-1].getLabel();
+		str += ") possui a distancia de: "+ way[1][i] + "<br>";
+		
+		//console.log( str );
 	}
 	
 	
+	document.getElementById("q2").innerHTML =  str ;
 	
-	
-	//A distância de A a C passando por B?
+	//A distância de A a C passando por D?
 	way = Start(a, c);
 	console.log("Q3 - A distância de A a C passando por D?");
-	for(var i = 0; i < way[0].length; i++){
-		
-		for(var j = 0; j < way[0][i].length; j++){
-			if(way[0][i][j] == b){
-				var str = "caminho "+ i + " (";
-				for(var l = 0; l < way[0][i].length-1; l++){
-					str += way[0][i][l].getLabel()+", ";
-				}
-				str += way[0][i][way[0][i].length-1].getLabel();
-				
-				str += ") possui a distancia de: "+ way[1][i];
-				
-				console.log( str );
-			}
-		}
-	}
-	
-	
-    //A distância de A a C passando por D?
-	way = Start(a, c);
-	console.log("Q4 - A distância de A a C passando por D?");
-	
-	for(var i = 0; i < way[0].length; i++){
-		for(var j = 0; j < way[0][i].length; j++){
-			if(way[0][i][j] == d){
-				var str = "caminho "+ i + " (";
-				for(var l = 0; l < way[0][i].length-1; l++){
-					str += way[0][i][l].getLabel()+", ";
-				}
-				str += way[0][i][way[0][i].length-1].getLabel();
-				
-				str += ") possui a distancia de: "+ way[1][i];
-				
-				console.log( str );
-			}
-		}
-	}	
+	str = "";
+	way_temp = getRouteToYPassingX(way, c, b);
+	document.getElementById("q3").innerHTML =  printRoutes(way_temp) ;
 	
     //O número de rotas saindo de C e voltando a C com no máximo 3 paradas?
 	way = Start(c, c);
-	console.log("Q5 - O número de rotas saindo de C e voltando a C com no máximo 3 paradas?");	
-	qtd = 0;
+	qtd = getCountRouteToYMaxStop(way, c, 3);
+	document.getElementById("q4").innerHTML = "O número de rotas entre C e C, com no máximo 3 paradas é "+  qtd  ;
+
 	
-	for(var i = 0; i < way[0].length; i++){
-		
-		if(way[0][i][ way[0][i].length-1 ] != c)
-		  continue;
-	  
-	  
-		if(way[0][i].length <= 3+2)
-			qtd++;
-	}
-	
-	console.log("O número de rotas entre C e C, com no máximo 3 paradas é "+qtd);
-	
-	
-    //O número de rotas entre A e C com no máximo 4 paradas?
-	way = Start(a, c);
-	console.log("Q6 - O número de rotas entre A e C com no máximo 4 paradas?");	
-	qtd = 0;
-	
-	for(var i = 0; i < way[0].length; i++){
-		
-		if(way[0][i][ way[0][i].length-1 ] != c)
-		  continue;
-	  
-	  
-		if(way[0][i].length <= 4 + 2)
-			qtd++;
-	}
-	
-	console.log("O número de rotas entre A e C, com no máximo 4 paradas é "+qtd);
 	
     //A menor rota (em espaço-tempo) entre A e C?
 	way = Start(a, c);
-	console.log("Q7 - A menor rota (em espaço-tempo) entre A e C?");
-	menor_rota = [Infinity, null];
-	for(var i = 0; i < way[0].length-1; i++){
+	qtd = getCountRouteToYMaxStop(way, c, 4);
+	document.getElementById("q5").innerHTML = "O número de rotas entre A e C, com no máximo 3 paradas é "+  qtd  ;
+
 	
-		if(way[0][i][way[0][i].length-1] != c)
-		  continue;
-		
-		if(way[1][i] < menor_rota[0]){
-			menor_rota = [way[1][i], way[0][i] ];
-		}
-		
-	}
+    //A menor rota (em espaço-tempo) entre A e C?
+	way = Start(a, c);
+	menor_rota = getLessRoute(way, c);
 	
 	str = "A menor rota de A a C é (";
 	for(var l = 0; l < menor_rota[1].length-1; l++){
 		str += menor_rota[1][l].getLabel()+", ";
 	}
 	
-	str += menor_rota[1][menor_rota[1].length-1].getLabel();
+	str += menor_rota[1][menor_rota[1].length-1].getLabel() +") com peso "+menor_rota[0];
 	
-	console.log(str + ") com peso "+menor_rota[0]);
+	document.getElementById("q6").innerHTML = str;
 	
 	
-    //A menor rota (em espaço-tempo) saindo de B e voltando a B?
+     //A menor rota (em espaço-tempo) saindo de B e voltando a B?
 	way = Start(b, b);
-	console.log("Q8 - A menor rota (em espaço-tempo) saindo de B e voltando a B?");	
-	menor_rota = [Infinity, null];
-	for(var i = 0; i < way[0].length-1; i++){
-	
-		if(way[0][i][way[0][i].length-1] != b)
-		  continue;
-		
-		if(way[1][i] < menor_rota[0]){
-			menor_rota = [way[1][i], way[0][i] ];
-		}
-		
-	}
+	menor_rota = getLessRoute(way, b);
+	console.log(menor_rota)
 	
 	str = "A menor rota de B a B é (";
 	for(var l = 0; l < menor_rota[1].length-1; l++){
 		str += menor_rota[1][l].getLabel()+", ";
 	}
 	
-	str += menor_rota[1][menor_rota[1].length-1].getLabel();
+	str += menor_rota[1][menor_rota[1].length-1].getLabel() +") com peso "+menor_rota[0];
 	
-	console.log(str + ") com peso "+menor_rota[0]);
+	document.getElementById("q7").innerHTML = str;
+	
+	
+	
 	
     //O número de diferentes rotas saindo de C e voltando a C com distância máxima de 300 unidades de espaço-tempo?
 	way = Start(c, c);
 	console.log("Q9 - O número de diferentes rotas saindo de C e voltando a C com distância máxima de 300 unidades de espaço-tempo?");
-	qtd = 0;
-	for(var i = 0; i < way[0].length; i++){
-		if(way[0][i][way[0][i].length-1] != c)
-		  continue;
-		if(way[1][i] <= 300)
-			qtd++;
-		
-	}
-	console.log("O número de rotas entre C e C, com no máximo 300 unidades de espaço-tempo é "+qtd);
+	qtd = getCountRouteToYMaxDistance(way, 300, c);
+	
+	str = "O número de rotas entre C e C, com no máximo 300 unidades de espaço-tempo é "+qtd;
+	
+	document.getElementById("q8").innerHTML = str; 
 	
 	//console.log(way);
 	
 }
 
 startApp();
+
+
+
+function getRouteToYPassingX (way, y, x){
+	way_temp = [[],[]];
+	for(var i = 0; i < way[0].length; i++){	
+		if(way[0][i][ way[0][i].length-1 ] != y)
+		  continue;
+	  
+		for(var j = 0; j < way[0][i].length; j++){
+			if(way[0][i][j] == x){
+				way_temp[0].push(way[0][i]);
+				way_temp[1].push(way[1][i]);
+				continue;
+			}
+		}
+	}	
+
+	return way_temp;	
+}
+
+function getCountRouteToYMaxStop(way_temp, y, max){
+	qtd = 0;
+	
+	for(var i = 0; i < way_temp[0].length; i++){
+		
+		if(way_temp[0][i][ way_temp[0][i].length-1 ] != c)
+		  continue;
+	  
+	  
+		if(way_temp[0][i].length <= max+2)
+			qtd++;
+	}
+	
+	return qtd;
+
+}
+
+
+function getCountRouteToYMaxDistance(way_temp, max, x){
+	qtd = 0;
+	for(var i = 0; i < way[0].length; i++){
+		if(way[0][i][way[0][i].length-1] != x)
+		  continue;
+	  
+		if(way[1][i] <= max)
+			qtd++;
+		
+	}
+	return qtd;
+}
+
+function getLessRoute(way_temp, x){
+	
+	less_route = [Infinity, null];
+	
+	for(var i = 0; i < way_temp[0].length-1; i++){
+	   
+		if(way_temp[0][i][ way_temp[0][i].length-1 ] != x)
+		  continue;
+	  
+		if(way_temp[1][i] < less_route[0]){
+			less_route = [way_temp[1][i], way_temp[0][i] ];
+		}
+		
+	}
+	
+	return less_route;
+	
+}
+	
+	
+function printRoutes(way_temp){
+	str="";
+	for(var j = 0; j < way_temp[0].length; j++){
+		str += "caminho "+ j + " (";
+			
+			for(var l = 0; l < way_temp[0][j].length-1; l++){
+				str += way_temp[0][j][l].getLabel()+", ";
+			}
+			
+			str += way_temp[0][j][way_temp[0][j].length-1].getLabel();
+		
+		
+		str += ") possui a distancia de: "+ way_temp[1][j] +"<br>";
+	}
+	
+	return str;
+}
